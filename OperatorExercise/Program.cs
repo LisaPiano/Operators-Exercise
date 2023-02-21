fbf3839d2e4e7fd9ef91e4e167961250a4dc41b7
Lisa Walters <lisacampiwalters@gmail.com>
2023-02-20 20:41:51 -0700

    added more mathematical operations

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,7 +8,10 @@
             int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Thanks! Now, would you please give me one more number? Type it here:");
             int num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Thanks again! So, if we add your two numbers, the result is {num1 + num2}");
+            Console.WriteLine($"Thanks again! So, if we add your two numbers, the result is {num1 + num2}.");
+            Console.WriteLine($"If we subtract your first number from your second number, the result is {num2-num1}.");
+            Console.WriteLine($"If we multiply your two numbers together, the result is {num1 * num2}.");
+            Console.WriteLine($"If we divide the first number by the second number the remainer is {num1 % num2}.");
 
         }
     }
