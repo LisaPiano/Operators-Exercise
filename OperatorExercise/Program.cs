523ebe0c06463cdf5beb9f71363756d246d27c3c
Lisa Walters <lisacampiwalters@gmail.com>
2023-02-20 20:36:43 -0700

    initial commit, and created first addition operation

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello! Would you please give me one number? Type it here:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Thanks! Now, would you please give me one more number? Type it here:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Thanks again! So, if we add your two numbers, the result is {num1 + num2}");
+
         }
     }
 }
